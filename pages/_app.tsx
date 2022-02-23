dae7088b92370e00423280424e46347fe59fb200
Vladimir Topolev <i.topolev.vladimir@gmail.com>
2022-02-23 16:37:36 +0300

    Init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app';
 import { wrapper } from '../store';
-
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          name='google-site-verification'
+          content='vaAaDH_-3EJGyAe78gu1ysTNYEhIkosQZytMoORLuvY'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
